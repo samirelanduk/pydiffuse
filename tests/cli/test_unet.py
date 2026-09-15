6ca4abe94e5be219d6ddb5c3f620137e7db297e8
Sam Ireland <mail@samireland.com>
2026-09-15 20:35:05 +0100

    Initial UNet CLI test

--- tests/cli/test_unet.py
+++ tests/cli/test_unet.py
@@ -40,13 +40,24 @@ class UnetPredictTestCase(UnetTestCase):
     def create_latent(self):
         latent_path = self.test_dir / "latent.pt"
         torch.manual_seed(42)
-        torch.save(torch.randn(4, 8, 8), latent_path)
+        torch.save(torch.randn(4, 7, 9), latent_path)
         return latent_path
 
     def run_command(self, *args, **kwargs):
         return super().run_command("predict", *args, **kwargs)
 
-    def test(self):
+    def check_noise(self, noise):
+        self.assertEqual(noise.shape, (4, 7, 9))
+        self.assertEqual(round(noise[0, 0, 0].item(), 3), 0.179)
+        self.assertEqual(round(noise[0, 0, 8].item(), 3), -0.032)
+        self.assertEqual(round(noise[0, 6, 0].item(), 3), 0.061)
+        self.assertEqual(round(noise[0, 6, 8].item(), 3), 0.115)
+        self.assertEqual(round(noise[3, 0, 0].item(), 3), -0.262)
+        self.assertEqual(round(noise[3, 0, 8].item(), 3), -0.432)
+        self.assertEqual(round(noise[3, 6, 0].item(), 3), -0.116)
+        self.assertEqual(round(noise[3, 6, 8].item(), 3), 0.042)
+
+    def test_half_noise(self):
         # Run the command with only the required arguments
         result = self.run_command(
             self.latent_path, "0.5", self.conditioning_path, self.model_path
@@ -59,3 +70,8 @@ class UnetPredictTestCase(UnetTestCase):
 
         # File is created
         self.assertTrue((self.test_dir / "noise.pt").exists())
+
+        # Prediction is correct
+        with open(self.test_dir / "noise.pt", "rb") as f:
+            noise = torch.load(f)
+        self.check_noise(noise)
